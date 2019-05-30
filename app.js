cbdfae0c7f111cd27f4dc574f9a23a36c239f40b
Jakz Aizzat <jakzaizzat@gmail.com>
2019-05-30 16:30:35 +0800

    Normalize the data

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var port = process.env.PORT || 3000;
 
-var axios = require("axios");
 var dotenv = require("dotenv").config();
 
 var Twitter = require("twitter");
@@ -14,8 +13,6 @@ var client = new Twitter({
   bearer_token: process.env.TWITTER_BEARER_TOKEN
 });
 
-//https://cors-anywhere.herokuapp.com/
-
 app.get("/", function(req, res) {
   var tweetsArr = [];
   client.get("search/tweets", { q: "$goog" }, function(
@@ -24,21 +21,15 @@ app.get("/", function(req, res) {
     response
   ) {
     tweets.statuses.forEach(function(tweet) {
-      //console.log("tweet: " + tweet.text);
-      console.log(tweet);
-
       var tweet = {
         profile: tweet.user.profile_image_url,
         username: tweet.user.screen_name,
         message: tweet.text
       };
-
       tweetsArr.push(tweet);
     });
     res.send(tweetsArr);
   });
-
-  //res.send(JSON.stringify({ name: "aizzat" }));
 });
 
 app.listen(port, function() {
